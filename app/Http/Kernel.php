1bbc5059df5535db02b2c7b44bd44b454645193d
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-08-28 15:11:33 +0300

    Server <> created middlewares

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,6 +54,9 @@ class Kernel extends HttpKernel
      */
     protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'auth.admin' => \App\Http\Middleware\AuthenticateAdmin::class,
+        'auth.worker' => \App\Http\Middleware\AuthenticateWorker::class,
+        'auth.partner' => \App\Http\Middleware\AuthenticatePartner::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,25 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
+Route::post("/login", [AuthController::class, "login"]);
+Route::post("/register", [AuthController::class, "register"]);
+
+
+Route::group(["middleware" => "auth:api"], function(){
+    Route::group(["middleware" => "auth.admin", "prefix" => "admin"], function(){
+
+    });
+
+    Route::group(["middleware" => "auth.worker", "prefix" => "worker"], function(){
+
+    });
+
+    Route::group(["middleware" => "auth.partner", "prefix" => "partner"], function(){
+
+    });
+});
+
+
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('register', 'register');
