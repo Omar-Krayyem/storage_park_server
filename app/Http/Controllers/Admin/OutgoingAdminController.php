745e1900eeb5b58d614ea665f3864775c8499c5c
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-09-10 12:44:52 +0300

    Server <> created shipment outgoing functions for admin

--- app/Http/Controllers/Admin/OutgoingAdminController.php
+++ app/Http/Controllers/Admin/OutgoingAdminController.php
@@ -93,6 +93,58 @@ class OutgoingAdminController extends Controller
         }
     }
 
+    public function getAllShipment(){
+        try{
+            $orders = Order::where('order_type_id', 2)->where('status', 'shipment')->with(['user', 'customer', 'worker'])->get();
+
+            return $this->customResponse($orders, 'success', 200);
+        }catch(Exception $e){
+            return self::customResponse($e->getMessage(),'error',500);
+        }
+    }
+
+    public function shipmentSearch($requestSearch) {
+        try {
+            $orders = Order::with(['user', 'customer', 'worker'])
+                ->where('status', 'shipment')
+                ->where('order_type_id', 2)
+                ->where(function ($query) use ($requestSearch) {
+                    $query->where('id', 'LIKE', "%$requestSearch%")
+                        ->orWhere('placed_at', 'LIKE', "%$requestSearch%")
+                        ->orWhereHas('user', function ($userQuery) use ($requestSearch) {
+                            $userQuery->where('company_name', 'LIKE', "%$requestSearch%");
+                        })
+                        ->orWhereHas('worker', function ($workerQuery) use ($requestSearch) {
+                            $workerQuery->where('first_name', 'LIKE', "%$requestSearch%")
+                                        ->orWhere('last_name', 'LIKE', "%$requestSearch%");
+                        })
+                        ->orWhereHas('customer', function ($workerQuery) use ($requestSearch) {
+                            $workerQuery->where('name', 'LIKE', "%$requestSearch%");
+                        });
+                })
+                ->get();
+    
+            return $this->customResponse($orders);
+        } catch (Exception $e) {
+            return self::customResponse($e->getMessage(), 'error', 500);
+        } 
+    }
+
+    public function getShipmentById(Order $order){
+        try{
+            $order = Order::with([
+                'worker',
+                'orderItems.product.category',
+                'user', 
+                'customer'
+            ])->find($order->id);
+
+            return $this->customResponse($order, 'success', 200);
+        }catch(Exception $e){
+            return self::customResponse($e->getMessage(),'error',500);
+        }
+    }
+
     function customResponse($data, $status = 'success', $code = 200){
         $response = ['status' => $status,'data' => $data];
         return response()->json($response,$code);
