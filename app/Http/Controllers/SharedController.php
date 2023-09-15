82cc3675947b2fa31851a66c2517372993603203
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-09-15 11:50:36 +0300

    Server <> updated the get admin stat api

--- app/Http/Controllers/SharedController.php
+++ app/Http/Controllers/SharedController.php
@@ -22,29 +22,30 @@ class SharedController extends Controller
                 'last_name' => ['required', 'string'],
                 'phone' => ['required', 'string'],
                 'email' => ['required', 'string'],
-                'password' => ['required', 'string', 'min:6'],
+                'password' => ['string', 'min:6', 'nullable'],
                 'address' => ['required', 'string'],
                 'company_name' => ['string', 'nullable'],
             ]);
-
+    
             $user = User::find($user_id);
-
+    
             $user->first_name = $validated_data['first_name'];
             $user->last_name = $validated_data['last_name'];
             $user->email = $validated_data['email'];
             $user->phone = $validated_data['phone'];
             $user->address = $validated_data['address'];
-
-            $password = Hash::make($validated_data['password']);
-            $user->password = $password;
-
-            $user->company_name = null;
+    
+            if (array_key_exists('password', $validated_data)) {
+                $password = Hash::make($validated_data['password']);
+                $user->password = $password;
+            }
+    
             if (array_key_exists('company_name', $validated_data)) {
                 $user->company_name = $validated_data['company_name'];
             }
-
+    
             $user->save();
-
+    
             return $this->customResponse($user, 'Updated Successfully');
         } catch (Exception $e) {
             return self::customResponse($e->getMessage(),'error',500);
