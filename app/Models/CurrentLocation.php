588482d2613cc4323fe6cec459b7ab36fb296708
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-09-25 17:26:07 +0300

    Server <> created relation to current location model

--- app/Models/CurrentLocation.php
+++ app/Models/CurrentLocation.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class CurrentLocation extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public $timestamps = false;
+
+    public function worker(){
+        return $this->belongsTo(User::class,'worker_id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,8 @@ class User extends Authenticatable implements JWTSubject
     public function stocks(){
         return $this->hasMany(Stock::class, 'user_id');
     }
+
+    public function location(){
+        return $this->hasOne(CurrentLocation::class, 'worker_id');
+    }
 }
