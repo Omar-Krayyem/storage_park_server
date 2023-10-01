377cac14c70e36e4c1987f4cd0381ba50981e32e
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-10-01 03:29:31 +0300

    Server <> updated incoming order apis for worker

--- app/Http/Controllers/Worker/IncomingWorkerController.php
+++ app/Http/Controllers/Worker/IncomingWorkerController.php
@@ -18,7 +18,7 @@ class IncomingWorkerController extends Controller
         try{
             $worker_id = Auth::user()->id;
 
-            $orders = Order::where('worker_id', $worker_id)->where('order_type_id', 1)->where('status', 'shipment')->with(['user' , 'orderItems.product.category'])->get();
+            $orders = Order::where('worker_id', $worker_id)->where('order_type_id', 1)->with(['user' , 'orderItems.product.category'])->get();
 
             $orders= $orders->map(function ($order) {
                 $order->item_count = $order->orderItems->count();
@@ -35,11 +35,11 @@ class IncomingWorkerController extends Controller
         try {
             $worker_id = Auth::user()->id;
             $orders = Order::with(['user', 'worker'])
-                ->where('status', 'shipment')
                 ->where('order_type_id', 1)
                 ->where('worker_id', $worker_id)
                 ->where(function ($query) use ($requestSearch) {
                     $query->where('id', 'LIKE', "%$requestSearch%")
+                        ->orwhere('status', 'LIKE', "%$requestSearch%")
                         ->orWhere('placed_at', 'LIKE', "%$requestSearch%")
                         ->orWhereHas('user', function ($userQuery) use ($requestSearch) {
                             $userQuery->where('company_name', 'LIKE', "%$requestSearch%");
